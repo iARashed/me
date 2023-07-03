c82c64e2b9a1a1469e4d4cb4068dff4b8c494251
Ahmed H. Rashed <iARashed@users.noreply.github.com>
2023-07-03 16:47:39 -0700

    Update addme.js

--- js/addme.js
+++ js/addme.js
@@ -1,20 +1,6 @@
-var saveBtn = document.getElementById("add-contacts");
-saveBtn.addEventListener("click", function () {
-  // Get the contact information from the website
-  var contact = {
-    name: "Ahmed Rashed",
-    phone: "+201002825028",
-    email: "iarashed@live.com"
-  };
-  // create a vcard file
-  var vcard = "BEGIN:VCARD\nVERSION:4.0\nFN:" + contact.name + "\nTEL;TYPE=work,voice:" + contact.phone + "\nEMAIL:" + contact.email + "\nEND:VCARD";
-  var blob = new Blob([vcard], { type: "text/vcard" });
-  var url = URL.createObjectURL(blob);
-  
-  const newLink = document.createElement('a');
-  newLink.download = contact.name + ".vcf";
-  newLink.textContent = contact.name;
-  newLink.href = url;
-  
-  newLink.click();
-});
+// Change "{}" to your options:
+// https://github.com/sampotts/plyr/#options
+const player = new Plyr('audio', {});
+
+// Expose player so it can be used from the console
+window.player = player;
